7d473746756e339cbd8198e1300b5cfddb48fb06
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-02-04 16:35:38 +0900

    #5 子ビューの定義

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrapの定形コード… -->
+
+    <div class="panel-body">
+        <!-- バリデーションエラーの表示 -->
+        @include('common.errors')
+
+        <!-- 新タスクフォーム -->
+        <form action="/task" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <!-- タスク名 -->
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Task</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
+                </div>
+            </div>
+
+            <!-- タスク追加ボタン -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> タスク追加
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- TODO: 現在のタスク -->
+@endsection
