86e22da669e83598d29758cce7d5dfecc1144ae1
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-02-04 16:28:06 +0900

    #4 ルートのスタブを定義

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * 全タスク表示
+ */
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * 既存タスク削除
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
 });
